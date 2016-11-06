2dc675ad09f835b2c56758f61fda82f889fd6a52
alejandroaldana <alejandroaldana@outlook.com>
2016-11-06 19:14:21 +0100

    Comment

--- YandexTranslateCSharp/Models/YanderResultForLang.cs
+++ YandexTranslateCSharp/Models/YanderResultForLang.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//NAMESPACE
 namespace YandexTranslateCSharp.Models
 {
     public class YanderResultForLang
